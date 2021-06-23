c975265a2fdb361866ab9f60dcc0d701733365c3
jaredfairchild <jaredfairchild@gmail.com>
2021-06-22 21:32:28 -0600

    Adding files for Step 66 assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+var hello = "Hello";
+var js = "JavaScript";
+
+// Concatenate the String variables and insert a space between them
+var msg = hello + " " + js;
+window.alert(msg);
+
+var greenMsg = msg.fontcolor("green"); // The message in green
+document.write(greenMsg); // Write the green message to the screen
