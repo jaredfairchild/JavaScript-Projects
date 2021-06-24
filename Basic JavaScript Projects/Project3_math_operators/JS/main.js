4764287e5b18df14ede83141bb7fd321c8b2cde2
jaredfairchild <jaredfairchild@gmail.com>
2021-06-23 19:49:22 -0600

    Added functions per step 97

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,8 +1,50 @@
+// Function using +
 function add() {
-	// Get the 2 opperands and convert them from strings to integers
-	var num2 = parseInt(document.getElementById("num2").innerText);
-	var num1 = parseInt(document.getElementById("num1").innerText);
+	// 10 + 50
+	var res = 10 + 50;
+	document.getElementById("addResult").innerHTML = res;
+}
 
-	// Add the numbers together and put the result on the screen
-	document.getElementById("result").innerHTML = num1 + num2;
+// Function using -
+function subtract() {
+	// 50 - 10
+	var res = 50 - 10;
+	document.getElementById("subResult").innerHTML = res;
+}
+
+// Function using *
+function multiply() {
+	// 20 x 50
+	var res = 20 * 50;
+	document.getElementById("multResult").innerHTML = res;
+}
+
+// Function using %
+function modulus() {
+	// 7 / 2
+	var res = Math.floor(7 / 2).toString() + " with a remainder of " + (7 % 2);
+	document.getElementById("moduloResult").innerHTML = res;
+}
+
+// Function using ++
+function increment() {
+	// 10++
+	var res = 10;
+	res++;
+	document.getElementById("incResult").innerHTML = res;
+}
+
+// Function using --
+function decrement() {
+	// 10--
+	var res = 10;
+	res--;
+	document.getElementById("decResult").innerHTML = res;
+}
+
+// Function using Math.random()
+function rando() {
+	document.getElementById("randoResult").innerHTML = Math.floor(
+		Math.random() * 100
+	);
 }
