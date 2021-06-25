fe9cca6c2382ffcb63391ca0ca8e7a9b543be2f5
jaredfairchild <jaredfairchild@gmail.com>
2021-06-24 23:25:12 -0600

    Ready to complete step 142

---
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -0,0 +1,9 @@
+// Write a function using HTML and JavaScript that utilizes a ternary operation using input from the browser
+
+// Write a constructor function that uses the “new” and “this” keywords
+
+// Create a function to display the results of the constructor function within an HTML element
+
+// Write a nested function
+
+// Add comments throughout your JavaScript explaining your code
